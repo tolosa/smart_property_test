bd8abc1206bcb50ab6e27349c58e4c17e4001d7a
Leonardo Tolosa <tolosa@gmail.com>
2020-01-19 16:11:01 -0300

    Add trait to create associated units

--- test/factories/properties.rb
+++ test/factories/properties.rb
@@ -2,6 +2,12 @@ FactoryBot.define do
   factory :property do
     name { Faker::Company.name }
     description { Faker::Company.catch_phrase }
-    address { Faker::Address.address }
+    address { Faker::Address.street_address }
+
+    trait :with_units do
+      after(:create) do |property, evaluator|
+        create_list(:unit, Faker::Number.within(range: 1..5), property: property)
+      end
+    end
   end
 end

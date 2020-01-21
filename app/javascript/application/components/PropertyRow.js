163992f4b42d8bfbe99500aaf1f9671c74b13341
Leonardo Tolosa <tolosa@gmail.com>
2020-01-21 00:07:22 -0300

    Add units nested table list [WIP]

--- app/javascript/application/components/PropertyRow.js
+++ app/javascript/application/components/PropertyRow.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
+import UnitsTable from './UnitsTable'
+
 const PropertyRow = props => {
   return (
-    <tr>
-      <td>{props.property.name}</td>
-      <td>{props.property.description}</td>
-      <td>{props.property.address}</td>
-    </tr>
+    <>
+      <tr>
+        <td>{props.property.name}</td>
+        <td>{props.property.description}</td>
+        <td>{props.property.address}</td>
+      </tr>
+      <tr>
+        <td colspan="3">
+          <UnitsTable units={props.property.units} />
+        </td>
+      </tr>
+    </>
   )
 }
 
---
+++ app/javascript/application/components/UnitsTable.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const UnitsTable = props => {
+  return (
+    <table className="table table-sm table-bordered">
+      <thead>
+        <tr>
+          <th className="w-25">Number</th>
+          <th>Area</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.units.map((unit, index) =>
+          <tr>
+            <td>{unit.number}</td>
+            <td>{unit.area}</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  )
+}
+
+export default UnitsTable

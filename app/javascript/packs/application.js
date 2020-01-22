27f7a85a7d4356585e74db7cd35bf37c465e7729
Leonardo Tolosa <tolosa@gmail.com>
2020-01-22 12:01:36 -0300

    Add global axios configuration

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 
 import Index from '../application/index'
+
+axios.defaults.baseURL = '/api/v1'
+axios.defaults.headers.common['Authorization'] = "Token token=Rq0Sffmyre4pXxOoynCc5r2GkKZBLL_ZEC49mybX-5VUzSlcRqQgcTAkjzPMOotPO5Z52NcLKJDWN5fCsG944A"
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(

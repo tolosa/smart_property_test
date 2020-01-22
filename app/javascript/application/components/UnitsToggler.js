f8dcf203210fc16a5fadf7e21472677fcfd4aef0
Leonardo Tolosa <tolosa@gmail.com>
2020-01-22 13:46:53 -0300

    Disable units toggler if empty

--- app/javascript/application/components/UnitsToggler.js
+++ app/javascript/application/components/UnitsToggler.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const UnitsToggler = props => {
-  const icon = props.expanded ? 'fa-minus-square' : 'fa-plus-square'
+  const isEnabled = !!props.unitsCount
+  const icon = isEnabled ? (props.expanded ? 'fa-minus-square' : 'fa-plus-square') : 'fa-square'
 
   return (
-    <a href="#" onClick={props.onClick}>
+    <a href={isEnabled && '#'} onClick={isEnabled && props.onClick} className={!isEnabled && 'text-muted'}>
       <i className={`fa mr-1 ${icon}`}></i>
       Units
       <small className="text-muted ml-1">({props.unitsCount})</small>

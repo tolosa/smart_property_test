773f3a7508f0d684d2bf859754d6dddff0c5bd22
Sebastian Segura <segurasebastian7887@gmail.com>
2020-01-23 15:57:29 -0300

    Fix DOM errors on UnitsToggler component

--- app/javascript/application/components/PropertiesContainer/UnitsToggler.js
+++ app/javascript/application/components/PropertiesContainer/UnitsToggler.js
@@ -3,9 +3,10 @@ import React from 'react'
 const UnitsToggler = props => {
   const isEnabled = !!props.unitsCount
   const icon = isEnabled ? (props.expanded ? 'fa-minus-square' : 'fa-plus-square') : 'fa-square'
+  const className = !isEnabled ? 'text-muted' : ''
 
   return (
-    <a href={isEnabled && '#'} onClick={isEnabled && props.onClick} className={!isEnabled && 'text-muted'}>
+    <a href="#" onClick={isEnabled ? props.onClick : () => {}} className={className}>
       <i className={`fa mr-1 ${icon}`}></i>
       Units
       <small className="text-muted ml-1">({props.unitsCount})</small>

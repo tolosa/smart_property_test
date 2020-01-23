a658346ed8615c6190047662d74da06007353494
Leonardo Tolosa <tolosa@gmail.com>
2020-01-23 13:48:33 -0300

    Increase limit to prevent unique error

--- test/factories/units.rb
+++ test/factories/units.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :unit do
-    number { Faker::Number.unique.within(range: 1..50) }
+    number { Faker::Number.unique.within(range: 1..150) }
     area { Faker::Address.community }
     property
   end
